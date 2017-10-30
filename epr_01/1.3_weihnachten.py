bb7b86814772339ad6b5df09a835fe35f01075db
Fjodorson <33236844+Fjodorson@users.noreply.github.com>
2017-10-31 00:08:12 +0100

    None-brute forcing solution
    Little humble jizz is in order, I think

--- epr_01/1.3_weihnachten.py
+++ epr_01/1.3_weihnachten.py
@@ -19,3 +19,31 @@ def days_till_chrismas(day, month):
 d = days_till_chrismas(14, 2)
 
 print(d)
+
+'''
+Solution works by comparing user input date with relevant christmas of the user input year.
+Admittedly stolen from the Python Standard Library. Countdown to event example was reworked and applied
+for the case at hand.
+'''
+__author__ = "5184262: Dominik Ploner"
+# Importing the modules necessary for this programme.
+import time
+from time import mktime
+import datetime
+from datetime import date  # Importing the modules necessary for this programme.
+# User enters a date. This results in an input str(). Needs to be converted.
+user_date = input("Please enter the date in the format DDMMYYYY: ")
+# Converting input(str) into time.time_struct object.
+user_date_parsed = time.strptime(user_date, "%d%m%Y")
+# Convert time.time_struct object into datetime.
+user_date_parsed_2 = date.fromtimestamp(mktime(user_date_parsed))
+# Define christmas variable. Take year from input str().
+christmas = date(user_date_parsed.tm_year, 12, 25)
+# The loop comparing relevant christmas with user input.
+if christmas == user_date_parsed_2:
+    print("M E R R Y    C H R I S T M A S!")
+# The case in which the user input is after December, 25th. 
+elif christmas < user_date_parsed_2:
+    christmas = christmas.replace(year=user_date_parsed.tm_year + 1)
+days_to_christmas = abs(christmas - user_date_parsed_2)
+print(days_to_christmas.days)
